5d25c2d2ae4c8a16da34329c104a3d6851469fde
shelepuginivan <shelepuginivanm@gmail.com>
2026-03-12 22:11:43 +0300

    feat: add linear algebra utils

---
+++ src/linear.c
@@ -0,0 +1,16 @@
+#include "linear.h"
+
+int invert_2x2(const double m[2][2], double inv[2][2]) {
+    double det = m[0][0] * m[1][1] - m[0][1] * m[1][0];
+    if (fabs(det) < 1e-12) {
+        return -1;
+    }
+
+    double inv_det = 1.0 / det;
+    inv[0][0] = m[1][1] * inv_det;
+    inv[0][1] = -m[0][1] * inv_det;
+    inv[1][0] = -m[1][0] * inv_det;
+    inv[1][1] = m[0][0] * inv_det;
+
+    return 0;
+}
---
+++ src/linear.h
@@ -0,0 +1,8 @@
+#ifndef DP_LINEAR_H
+#define DP_LINEAR_H
+
+#include <math.h>
+
+int invert_2x2(const double m[2][2], double inv[2][2]);
+
+#endif /* DP_LINEAR_H */
