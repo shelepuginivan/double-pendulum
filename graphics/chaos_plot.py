36bbac41503f7083268577037b5fa9ab93c4e4b0
shelepuginivan <shelepuginivanm@gmail.com>
2026-03-15 02:11:32 +0300

    feat(graphics): add configuration for chaos plot

--- graphics/chaos_plot.py
+++ graphics/chaos_plot.py
@@ -1,31 +1,52 @@
 import sys
+from dataclasses import dataclass
 
 import matplotlib
 
 matplotlib.use("Agg")
 
 import matplotlib.pyplot as plt
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
-def plot_csv(csv_file: str, color: str) -> None:
-    with open(csv_file) as f:
-        for i, line in enumerate(f):
-            if i % 5000 != 0:
-                continue
+class Config(BaseSettings):
+    framerate: int
+    dt: float
+    colors: list[str] = ["red", "blue", "green", "orange", "purple", "brown"]
+    plot_output: str = "chaos.png"
 
-            _, _, x2, y2 = map(float, line.split(","))
-            plt.scatter(x2, y2, marker="o", c=color, s=1)
+    model_config = SettingsConfigDict(env_prefix="DP_GRAPHICS_")
+
+
+@dataclass
+class Dataset:
+    csv_file: str
+
+    def plot(self, cfg: Config, color: str) -> None:
+        frame_duration = 1 / cfg.framerate
+        rows_per_frame = int(frame_duration / cfg.dt)
+
+        with open(self.csv_file) as csv:
+            for idx, row in enumerate(csv):
+                if idx % rows_per_frame != 0:
+                    continue
+
+                _, _, x2, y2 = map(float, row.split(","))
+                plt.scatter(x2, y2, color=color, marker="o", s=1)
 
 
 def main() -> None:
+    cfg = Config()  # type: ignore
     csv_files = sys.argv[1:]
 
     plt.title("Double Pendulum Divergence")
+    plt.xlabel("x")
+    plt.ylabel("y")
 
-    for csv in csv_files:
-        plot_csv(csv, "red")
+    for csv, color in zip(csv_files, cfg.colors):
+        Dataset(csv).plot(cfg, color)
 
-    plt.savefig("chaos.png")
+    plt.savefig(cfg.plot_output)
 
 
 if __name__ == "__main__":
