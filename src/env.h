58f48bb4040e5fa1488e94b56299d0827f0da98a
shelepuginivan <shelepuginivanm@gmail.com>
2026-03-20 07:55:36 +0300

    chore: remove unused env utilities

--- src/env.h
+++ src/env.h
@@ -4,7 +4,6 @@
 #include <stdlib.h>
 
 void dp_env_load_into_str(const char *name, char **dst);
-void dp_env_load_into_ulong(const char *name, unsigned long *dst);
 void dp_env_load_into_double(const char *name, double *dst);
 
 #endif /* DP_ENV_H */
